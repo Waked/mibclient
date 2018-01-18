fb69be93e9df4f14a1ca48b149a4a7bff47f905e
Waked <komma336@gmail.com>
2018-01-19 00:07:45 +0100

    Remove Webpack nonsense, add static libraries

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,10 +1,15 @@
 // import 'jquery/dist/jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../stylesheets/narrow-jumbotron.css';
-import 'bootstrap-treeview';
+import '../public/stylesheets/narrow-jumbotron.css';
+import 'bootstrap-treeview/dist/bootstrap-treeview.min.js';
+import 'bootstrap-treeview/dist/bootstrap-treeview.min.css';
 
 // Code here
+
+function click() {
+  alert($().jquery);
+}
 
 //const $ = jquery;
 
